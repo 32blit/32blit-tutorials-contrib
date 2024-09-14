a62de4262778d91440e7f9e2ae6b9acc4b734695
Charlie Birks <charlie@daft.games>
2024-09-14 18:13:54 +0100

    Lua version of ninja thief episode 1

---
+++ source-code/ninja-thief/episode-1/32blit-lua/constants.lua
@@ -0,0 +1,13 @@
+Constants = {
+    -- Screen size in pixels
+    SCREEN_WIDTH = 160,
+    SCREEN_HEIGHT = 120,
+
+    -- Actual game area in pixels
+    GAME_WIDTH = 120,
+    GAME_HEIGHT = 120,
+
+    -- Each sprite on the spritesheet is 8x8 pixels
+    SPRITE_SIZE = 8,
+}
+
